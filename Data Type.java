61613b54c765aca4225c8256b8cb92e9cf2866fa
Umair Javed <115348730+umairrrk24@users.noreply.github.com>
2022-10-16 17:12:41 +0500

    Add files via upload
    Data Type

---
+++ Data Type.java
@@ -0,0 +1,23 @@
+package data.types;
+
+public class DataTypes {
+
+   
+    public static void main(String[] args) {
+        
+        int n= 5; 
+        float f = 5.99f; 
+        double d= 53.99; 
+        char c = '['; 
+        boolean b= true; 
+        String s= "Hello"; 
+
+        System.out.println("Integer-Variable " + n);
+        System.out.println("Float-Variable " + f);
+        System.out.println("Double-Variable " + d);
+        System.out.println("Boolean-Variable " + b);
+        System.out.println("char-Variable " + c);
+        System.out.println("string-Variable " + s);
+    }
+    }
+    
